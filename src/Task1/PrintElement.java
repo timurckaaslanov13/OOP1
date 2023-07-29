01812275257c0a2b8b4848728606ab8e47cda27b
timurchik <timurckaaslanov@yandex.ru>
2023-07-29 14:40:15 +0300

    Added task1

---
+++ src/Task1/PrintElement.java
@@ -0,0 +1,13 @@
+package Task1;
+
+public class PrintElement<T> {
+    private T value;
+
+    public PrintElement(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
